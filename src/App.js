8ace3cace00eea448f75ada31da6434e389fc8bf
Xin Hu <hxmisty@gmail.com>
2018-07-11 17:32:52 -0400

    updated search

--- src/App.js
+++ src/App.js
@@ -1,47 +1,33 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import Bookshelves from '../../myreadings/src/Components/Bookshelves'
-import Search from '../../myreadings/src/Components/Search'
-import {Route} from 'react-router-dom'
-import './App.css'
-
+import React from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import * as BooksAPI from "./BooksAPI";
+import Bookshelves from "../../myreadings/src/Components/Bookshelves";
+import Search from "../../myreadings/src/Components/Search";
 
 class App extends React.Component {
-    state = {
+  state = {
+    books: [],
+  };
 
-        books:[]
+  componentDidMount() {
+    BooksAPI.getAll().then(data => {
+      console.log(data);
+      this.setState({
+        books: data,
+      });
+    });
+  }
 
+  render() {
+    return (
+      <div className="app">
+        <Route exact path="/" render={() => <Bookshelves booksOnShelf={this.state.books} />} />
 
-    }
-
-    componentDidMount(){
-
-        BooksAPI.getAll().then(
-            data => {
-
-                console.log(data)
-                this.setState({
-                    books:data
-                });
-            }
-        );
-    }
-
-
-    render() {
-        return (
-            <div className="app">
-
-                <Route exact path="/"
-                       render={()=>(<Bookshelves booksOnShelf={this.state.books}/>)}
-
-                />
-
-                <Route path="/search" render={()=>(<Search />)}/>
-
-            </div>
-        )
-    }
+        <Route path="/search" render={() => <Search booksInSearch={this.state.books} />} />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
